e15fd011973fe53951545bc0003fe99fe9d56bc3
Alexander <privetworld@gmail.com>
2024-12-07 03:06:35 +0300

    1. Настроил проект (скрыл секретный ключ, добавил пользовательские настройки). Установил зависимости (указаны в requirements.txt). Создал кастомную модель пользователей (одно расширешие, это уникальность email). Написал сериализатор и представление для регистрации и настроил маршруты, протестировал api с помощью postman.

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import RegistrationView
+
+
+app_name = 'accounts'
+
+
+urlpatterns = [
+    path('registration/', RegistrationView.as_view(), name='registration'),
+]
